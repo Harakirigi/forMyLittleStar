8266c74c1c425bb9fe9415120bac64f545e97220
Harakirigi <nkomyt2@gmail.com>
2025-03-23 16:15:17 +0500

    mark-icon.svg added

--- src/pages/QuestionEight.jsx
+++ src/pages/QuestionEight.jsx
@@ -5,9 +5,9 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/images/marker-icon-2x.png',
-  iconUrl: '/images/marker-icon.png',
-  shadowUrl: '/images/marker-shadow.png'
+  iconRetinaUrl: '/images/marker-icon-2x.svg',
+  iconUrl: '/images/marker-icon.svg',
+  shadowUrl: '/images/marker-shadow.svg'
 });
 
 
